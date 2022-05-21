8106a6195cda1cba8c7705f8044ec241b0186db4
Angus <Angus9823@163.com>
2022-05-21 18:01:12 +0800

    first commit

---
+++ src/main/java/com/angus/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.angus.git;
+
+/**
+ * Description :
+ * Created by Angus on 2022/5/21 17:55
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+
+    }
+}
