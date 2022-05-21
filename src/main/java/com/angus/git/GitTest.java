10c108cb1c13026b45e0348a270801cd930add9c
Angus <Angus9823@163.com>
2022-05-21 18:17:10 +0800

    hot-fix commit1

--- src/main/java/com/angus/git/GitTest.java
+++ src/main/java/com/angus/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git222!");
         System.out.println("hello git222!");
         System.out.println("hello git333!");
+        System.out.println("hello git444!");
 
     }
 }
